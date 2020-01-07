26264b4c68a29453259522549dfcfff1ea1c2980
M Rumman Hasan <mrummanhasan@gmail.com>
2020-01-07 16:01:20 +0500

    attr dir - ngStyle,ngClass

--- first-angularapp/src/app/directive-example/directive-example.component.ts
+++ first-angularapp/src/app/directive-example/directive-example.component.ts
@@ -13,13 +13,51 @@ export class DirectiveExampleComponent implements OnInit {
   toggleHero(){
     this.toggleHeroFlag=!this.toggleHeroFlag;
   }
-
   selectedHero:string;
   selectHero(hero)
   {
     this.selectedHero=hero;
     console.log(hero);
   }
+
+  //ngStyle
+  myStyle:{};
+  color:string;
+  styleToggleFlag:boolean=true;
+  setStyle(){
+    this.myStyle={
+      'font-weight': this.styleToggleFlag ? 'bold' : 'normal',
+      'font-size': this.styleToggleFlag ? '24px' : '15px',
+      'color': this.generateRandomColor()
+    }
+      this.styleToggleFlag = !this.styleToggleFlag;
+  }
+  generateRandomColor(){
+    this.color = "#";
+    var letter = ['0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'];
+    for(var i=0;i<6;i++)
+    {
+      this.color += letter[Math.floor(Math.random()*16)];
+    }
+    console.log(this.color);
+
+    return this.color;
+  }
+
+  //ngClass
+  currentCSSClass:string='styleRed';
+  setCSSClass(){
+    if (this.currentCSSClass == 'styleWhite') {
+      this.currentCSSClass = 'styleRed';
+    } else {
+      this.currentCSSClass = 'styleWhite';
+    }
+    
+console.log(this.currentCSSClass);
+  }
+
+
+
 
   constructor() { }
 
