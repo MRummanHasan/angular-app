17e518bf4c06cd1393927d1e89be6de8af868476
M Rumman Hasan <mrummanhasan@gmail.com>
2020-01-12 20:44:19 +0500

    template froms complete

--- template-driven-form/src/app/template-driven/template-driven.component.ts
+++ template-driven-form/src/app/template-driven/template-driven.component.ts
@@ -1,12 +1,20 @@
 import { Component, OnInit } from '@angular/core';
-
+import {User} from '../user';
+import { from } from 'rxjs';
 @Component({
   selector: 'app-template-driven',
   templateUrl: './template-driven.component.html',
   styleUrls: ['./template-driven.component.css']
 })
 export class TemplateDrivenComponent implements OnInit {
-
+  userList: User[]=[];
+  
+  addUser(form){
+    var user: User;
+    user=form.value;
+    this.userList.push(user);
+    console.log(form.value);
+  }
   constructor() { }
 
   ngOnInit() {
